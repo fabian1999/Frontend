90348a03f6332a2d9ca6a97268e9d39e39efd68e
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-23 11:49:56 +0300

    Added javascript to add to our table the user.

--- form.js
+++ form.js
@@ -3,8 +3,9 @@ function addFields(){
     var lastName = document.getElementById("lastName").value;
     var firstName = document.getElementById("firstName").value;
     var email = document.getElementById("email").value;
+    var date = document.getElementById("start").value;
     // Container <div> where dynamic content will be placed
     var list = document.getElementsByClassName("list");
 
-    document.getElementById('#table tr:last').after('<tr>...</tr><tr>...</tr>');
+    document.getElementById("table").insertRow(-1).innerHTML = '<tr><td>'+lastName+'</td><td>'+firstName+'</td><td>'+email+'</td><td>Sex</td><td>'+date+'</td></tr>';
 }
