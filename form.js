3ab937ee63522b97044384fc6e87a44d8cd851f7
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-23 12:07:35 +0300

    Added required functionality to the form.

--- form.js
+++ form.js
@@ -1,12 +1,29 @@
-function addFields(){
-    // Number of inputs to create
-    var lastName = document.getElementById("lastName").value;
-    var firstName = document.getElementById("firstName").value;
-    var email = document.getElementById("email").value;
-    var sex = document.getElementById("dropdown").value;
-    var date = document.getElementById("start").value;
-    // Container <div> where dynamic content will be placed
+function addFields() {
+  // Number of inputs to create
+  var lastName = document.getElementById("lastName").value;
+  var firstName = document.getElementById("firstName").value;
+  var email = document.getElementById("email").value;
+  var sex = document.getElementById("dropdown").value;
+  var date = document.getElementById("start").value;
+
+  if (
+    (lastName == "" || firstName == "" || email == "" || sex == "" || date == "")
+  ) {
+    alert("Fields are required.");
+  } else {
     var list = document.getElementsByClassName("list");
 
-    document.getElementById("table").insertRow(-1).innerHTML = '<tr><td>'+lastName+'</td><td>'+firstName+'</td><td>'+email+'</td><td>'+sex+'</td><td>'+date+'</td></tr>';
+    document.getElementById("table").insertRow(-1).innerHTML =
+      "<tr><td>" +
+      lastName +
+      "</td><td>" +
+      firstName +
+      "</td><td>" +
+      email +
+      "</td><td>" +
+      sex +
+      "</td><td>" +
+      date +
+      "</td></tr>";
+  }
 }
