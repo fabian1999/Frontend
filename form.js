42402a2b5146732bceac9ebceb44d312826dbc14
Bertea Fabian <fabian.bertea@yahoo.com>
2021-04-26 10:22:03 +0300

    Added search/filter functionallity.

--- form.js
+++ form.js
@@ -87,7 +87,7 @@ function myFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
+  table = document.getElementById("table");
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
